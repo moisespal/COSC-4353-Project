39d78d64a4a9199934b2e78fd2eea5fecbeae39e
Palacioem <palacio720@gmail.com>
2024-02-21 12:11:21 -0600

    validation is no longer inline and stops submit

--- logInScript.js
+++ logInScript.js
@@ -2,30 +2,31 @@
 function validationCheck(event) {
     var username = document.getElementById("username");
     var password = document.getElementById("password");    
-  
     // Example validation: Checking if both username and password are not empty
     if (username.value.trim() === "") {
         document.getElementById("errorMessage").textContent = "Please enter a username"
-        
+        event.preventDefault();
     }
 
     if (password.value.trim() === "") {
         document.getElementById("errorPassword").textContent = "Please enter a password"
-        
-    }
         event.preventDefault();
+    }
+  
+        
 }
 
 
-function revert(element){
-    element.style.borderColor = "black";
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("formLogin").addEventListener("submit", validationCheck)
 
-}
-
-document.getElementById("username").addEventListener("input", function() {
-    document.getElementById("errorMessage").textContent = "";
+    document.getElementById("username").addEventListener("input", function() {
+        document.getElementById("errorMessage").textContent = "";
+    });
+    document.getElementById("password").addEventListener("input", function() {
+        document.getElementById("errorPassword").textContent = "";
+    });
 });
 
-document.getElementById("password").addEventListener("input", function() {
-    document.getElementById("errorPassword").textContent = "";
-});
+
+
