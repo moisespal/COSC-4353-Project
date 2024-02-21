ef8c4c3931c039fad693e2988b0031f83f4b0692
Palacioem <palacio720@gmail.com>
2024-02-21 01:22:34 -0600

    added validation check for username and password

--- logInScript.js
+++ logInScript.js
@@ -1,2 +1,31 @@
 
-const inputField = document.getElementById('username')
+function validationCheck(event) {
+    var username = document.getElementById("username");
+    var password = document.getElementById("password");    
+  
+    // Example validation: Checking if both username and password are not empty
+    if (username.value.trim() === "") {
+        document.getElementById("errorMessage").textContent = "Please enter a username"
+        
+    }
+
+    if (password.value.trim() === "") {
+        document.getElementById("errorPassword").textContent = "Please enter a password"
+        
+    }
+        event.preventDefault();
+}
+
+
+function revert(element){
+    element.style.borderColor = "black";
+
+}
+
+document.getElementById("username").addEventListener("input", function() {
+    document.getElementById("errorMessage").textContent = "";
+});
+
+document.getElementById("password").addEventListener("input", function() {
+    document.getElementById("errorPassword").textContent = "";
+});
