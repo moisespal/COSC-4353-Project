ef940f4ccbd8d4ee9c4b78e56cce31d8f45c578d
Palacioem <palacio720@gmail.com>
2024-02-23 16:42:33 -0600

    validation function change

--- logInScript.js
+++ logInScript.js
@@ -1,25 +1,29 @@
 
-function validationCheck(event) {
-    var username = document.getElementById("username");
-    var password = document.getElementById("password");    
+function validationCheck(event) { //function validates that form has been filled before submiting 
+    var inputs = document.querySelectorAll(".input");
+    var errorMessage= document.querySelectorAll(".error");
+    var isvalid = true;
     // Example validation: Checking if both username and password are not empty
-    if (username.value.trim() === "") {
-        document.getElementById("errorMessage").textContent = "Please enter a username"
-        event.preventDefault();
+    for(let i=0; i<inputs.length; i++){
+        if(inputs[i].value.trim() === ""){
+            errorMessage[i].textContent = "Please enter a " + inputs[i].id;
+            isvalid = false;
+            event.preventDefault();
+        }
     }
-
-    if (password.value.trim() === "") {
-        document.getElementById("errorPassword").textContent = "Please enter a password"
-        event.preventDefault();
+    if (!isvalid) {
+        event.preventDefault(); // Prevent form submission
+    } else {
+        // Redirect to another page if validation passes
+        window.location.href = "/link";
     }
-  
-        
+ 
+    
 }
-
 
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("formLogin").addEventListener("submit", validationCheck)
-
+    //clear error message if usesr tries again 
     document.getElementById("username").addEventListener("input", function() {
         document.getElementById("errorMessage").textContent = "";
     });
